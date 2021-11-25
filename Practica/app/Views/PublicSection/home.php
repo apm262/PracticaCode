71b391d3665a803d2e637a6d4ed748a0c5ed2641
Alfonso <alfonsopm08@gmail.com>
2021-11-25 16:55:35 +0100

    Proyecto mas php base y links scripts

--- Practica/app/Views/PublicSection/home.php
+++ Practica/app/Views/PublicSection/home.php
@@ -1,12 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Home</h1>
-</body>
-</html>
+<?= $this->extend('base_layout') ?>
+
+<?=  $this->section('css')  ?>
+
+<?=  $this->endSection('css')  ?>
+
+
+<?=  $this->section('javascript')  ?>
+
+<?=  $this->endSection('javascript')  ?>
+
+
+
+
+
+<?=  $this->section('content')  ?>
+
+
+
+<?=  $this->endSection('content')  ?>
