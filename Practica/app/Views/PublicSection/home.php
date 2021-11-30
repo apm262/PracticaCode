dde6e2c418a9cb5b187ecccebfd8f708cfd93f01
Alfonso <alfonsopm08@gmail.com>
2021-11-30 16:59:17 +0100

    Empezando con home publico

--- Practica/app/Views/PublicSection/home.php
+++ Practica/app/Views/PublicSection/home.php
@@ -1,11 +1,17 @@
 <?= $this->extend('base_layout') ?>
 
 <?=  $this->section('css')  ?>
-
+<link rel="stylesheet" href="<?= base_url('assets/PublicSection/css/login.css') ?>"/>
 <?=  $this->endSection('css')  ?>
 
 
 <?=  $this->section('javascript')  ?>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+
+    });
+</script>
 
 <?=  $this->endSection('javascript')  ?>
 
@@ -15,6 +21,6 @@
 
 <?=  $this->section('content')  ?>
 
-
-
+    <h1 class="fest">Festivales</h1>
+    <h3>Indie</h3>
 <?=  $this->endSection('content')  ?>
